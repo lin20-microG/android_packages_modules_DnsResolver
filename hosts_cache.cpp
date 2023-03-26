3497db3e4ab091f2d07e13ec4bc63a233a324c0a
danielk43 <11856117+danielk43@users.noreply.github.com>
2023-03-26 11:37:26 -0400

    Reuse align_ptr in hosts_cache
    Change-Id: I461718c38ca3c0e372e1cc0e0f0deb74ae5c68bb
    
    Change-Id: I0fd439a000e7f8f341f25af9f2af182291f7c162
    Signed-off-by: MSe <mse1969@posteo.de>

--- hosts_cache.cpp
+++ hosts_cache.cpp
@@ -47,10 +47,6 @@ constexpr int MAXADDRS = 35;
 #define ESTIMATED_LINELEN	32
 #define HCFILE_ALLOC_SIZE	256
 
-/* From sethostent.c */
-#define ALIGNBYTES	(sizeof(uintptr_t) - 1)
-#define ALIGN(p)	(((uintptr_t)(p) + ALIGNBYTES) &~ ALIGNBYTES)
-
 /*
  * Host cache entry for hcfile.c_data.
  * Offsets are into hcfile.h_data.
@@ -500,8 +496,7 @@ int hc_gethtbyname(const char *host, int af, struct getnamaddr *info)
                 if (naliases >= MAXALIASES)
                     goto nospc;
             }
-            // NOLINTNEXTLINE: Allow casting to pointer
-            aligned = (char *)ALIGN(info->buf);
+            aligned = align_ptr(info->buf);
             if (info->buf != aligned) {
                 if ((ptrdiff_t)info->buflen < (aligned - info->buf))
                     goto nospc;
